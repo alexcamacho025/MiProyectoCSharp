f76050cd8bf7d603d104d36bce2688fcfa72b494
Alex Mauricio Camacho <alexcamacho0425@gmail.com>
2025-07-05 18:44:18 -0500

    Subiendo mi primer proyecto en C#

---
+++ Semana1_CalculadoraEdad/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Ingresa tu año de nacimiento: ");
+int aÑoNacimiento = int.Parse(Console.ReadLine());
+Console.Write("Ingresa el año actual: ");
+int aÑoActual = int.Parse(Console.ReadLine());
+int edad = aÑoActual - aÑoNacimiento;
+Console.WriteLine("Tu edad es: " + edad + " años.");
